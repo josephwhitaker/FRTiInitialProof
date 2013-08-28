8fd3202f45f1c02e40b345828f431c92c70f32bb
Vamsi Vidala <vamsi.vidala@forrent.com>
2013-08-28 13:00:10 -0400

    refactored search method

--- APP/libs/search.js
+++ APP/libs/search.js
@@ -1,4 +1,4 @@
-exports.search= function(lat, lon, radius, beds, budget, pageSize) {
+exports.search= function(lat, lon, callBack /*, radius, beds, budget, pageSize */) {
 	if(typeof lat === 'undefined' || typeof lon === 'undefined') {
 		return []; 
 	}
@@ -9,13 +9,24 @@ exports.search= function(lat, lon, radius, beds, budget, pageSize) {
    	budget = typeof budget !== 'undefined' ? budget : 99999;
    	pageSize = typeof pageSize !== 'undefined' ? pageSize : 20;
    	
-   	var xhr = Ti.Network.createHTTPClient();
-	xhr.onload = function() {
-		Ti.API.info(this.responseText);
-		callback(JSON.parse(this.responseText));
-	}
-	xhr.open('GET', 'http://bountyhunterapp.appspot.com/bounties');
-	xhr.send();
+	tempPos.latitude = inputLat;
+	tempPos.longitude = inputLong;
+	var url = Alloy.CFG.FRApi-SearchEndpoint + "ipad.php?latitude=" + lat + "&longitude=" + lon + "&radius=15&beds=99&maxPrice=9999&numberOfResults=20";
+	
+	var client = Ti.Network.createHTTPClient({
+		// function called when the response data is available
+		onload : function(e) {
+			callBack(JSON.parse(this.responseText));
+		},
+		// function called when an error occurs, including a timeout
+		onerror : function(e) {
+			Ti.API.debug(e.error);
+			alert('error');
+		},
+		timeout : 5000 // in milliseconds
+	});
+	client.open("GET", url);
+	client.send();
    	
    	
 	null;
